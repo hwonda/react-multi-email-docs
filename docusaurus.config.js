b9a5b5b44b0511ba617b67efddeffcfdb1a36cc5
hwonda <hwondaa@gmail.com>
2023-08-27 07:30:40 +0900

    fix: url 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://hwondaa.github.io',
+  url: 'https://hwonda.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-multi-email/',
