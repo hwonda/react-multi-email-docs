e5753319c91a74169a20b0621b211e00892b9542
hwonda <hwondaa@gmail.com>
2023-09-17 22:37:10 +0900

    docs: Add 'Essential Props' content

---
+++ src/examples/EssentialProps.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { ReactMultiEmail } from '../components/React-multi-email';
+import'../../src/components/React-multi-email/style.css';
+
+export default function EssentialProps () {
+  const [emails, setEmails] = React.useState<string[]>([]);
+  return (
+      <ReactMultiEmail
+        emails={emails}
+        onChange={(_emails: string[]) => {
+          setEmails(_emails);
+        }}
+        getLabel={(
+          email: string,
+          index: number,
+          removeEmail: (index: number) => void
+        ) => {
+          return (
+            <div data-tag key={index}>
+              {email}
+              <span data-tag-handle onClick={() => removeEmail(index)}>
+                ×
+              </span>
+            </div>
+          );
+        }}
+      />
+  );
+}
