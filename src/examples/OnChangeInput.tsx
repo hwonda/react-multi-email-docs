053b1eee4aa1bfc2a801b603cc1c6e90508ebabf
hwonda <hwondaa@gmail.com>
2023-09-21 21:53:37 +0900

    docs: Add prop 'onChangeInput'

---
+++ src/examples/OnChangeInput.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { ReactMultiEmail } from '../components/React-multi-email';
+import'../../src/components/React-multi-email/style.css';
+
+export default function multiEmail() {
+  const [emails, setEmails] = React.useState<string[]>([]);
+  const [inputStyle, setInputStyle] = React.useState<React.CSSProperties>({});
+
+  const handleInputChange = (value: string) => {
+    const isValidEmail = /\S+@\S+\.\S+/.test(value);
+    
+    if (!isValidEmail && value) {
+      setInputStyle({ borderColor: 'red' });
+    } else {
+      setInputStyle({});
+    }
+  };
+
+  return (
+    <div style={styles}>
+      <h3>react-multi-email</h3>
+      <ReactMultiEmail
+        onChangeInput={handleInputChange}
+        style={inputStyle}
+        emails={emails}
+        onChange={(_emails: string[]) => {
+          setEmails(_emails);
+        }}
+        getLabel={(
+          email: string,
+          index: number,
+          removeEmail: (index: number) => void
+        ) => {
+          return (
+            <div data-tag key={index}>
+              {email}
+              <span data-tag-handle onClick={() => removeEmail(index)}>
+                ×
+              </span>
+            </div>
+          );
+        }}
+      />
+      <h4>react-multi-email value</h4>
+      <p>{emails.join(", ") || "empty"}</p>
+    </div>
+  );
+}
+
+const styles = {
+  fontFamily: "sans-serif",
+  width: "500px",
+  border: "1px solid rgb(238, 238, 238)",
+  background: "rgb(243, 243, 243)",
+  padding: "25px",
+  margin: "20px",
+  color: "black",
+};
