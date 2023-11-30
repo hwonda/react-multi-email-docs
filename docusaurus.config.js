43869769db58804272ec3d30943cd8786976d549
hwonda <hwondaa@gmail.com>
2023-11-30 22:42:05 +0900

    chore: migration docusaurus v2 to v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,10 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -140,8 +142,8 @@ const config = {
         copyright: 'Copyright © 2023 axisj Inc.',
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
     }),
 };
